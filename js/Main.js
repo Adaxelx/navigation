662ca56d6dcae4a1d29ef9f8ed185ed0b4b8cd12
Adrian Mostowski <adaxelx@o2.pl>
2019-04-14 21:00:06 +0200

    add list to nav

--- js/Main.js
+++ js/Main.js
@@ -2,11 +2,23 @@ const bars = document.querySelectorAll('.bar')
 const navBtn = document.querySelector('.navBtn')
 const navBg = document.querySelector('.bigNavBg')
 const nav = document.querySelector('nav')
+const li = document.querySelectorAll('nav ul li')
+let flag = false;
+const addClassLi = () =>{
+    li.forEach(li => li.classList.toggle('active'))
+}
+
 const openNav = () => {
     navBtn.classList.toggle('active');
     bars.forEach(bar => bar.classList.toggle('active'))
     navBg.classList.toggle('active');
     nav.classList.toggle('active');
+    if(flag == true){
+        li.forEach(li => li.classList.toggle('active'))
+    }else{
+        setTimeout(addClassLi,1000)
+    }
+    flag = !flag;
 }
 
 navBtn.addEventListener('click',openNav)
